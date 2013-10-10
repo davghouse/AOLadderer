6485799f4aeb0fd1eaea7b823db81dab43788096
davghouse <davghouse@gmail.com>
2013-10-10 16:53:28 -0400

    Add pre-existing source
    Add source code developed prior to adoption of GitHub.

---
+++ Ladderer.pro
@@ -0,0 +1,36 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-10-01T12:57:11
+#
+#-------------------------------------------------
+ database.sources = "StandardImplants.db"
+ database.path = .
+ DEPLOYMENT += database
+QT       += core gui sql
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Ladderer
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    Stats.cpp \
+    Ladder.cpp \
+    Helper.cpp
+
+HEADERS  += mainwindow.h \
+    Stats.h \
+    Slot.h \
+    Ladder.h \
+    Implant.h \
+    Helper.h \
+    Config.h \
+    Coefficients.h
+
+FORMS    += mainwindow.ui
+
+OTHER_FILES += \
+    StandardImplants.db \
+    ladderImplants.txt
---
+++ Slot.h
@@ -0,0 +1,33 @@
+#ifndef GUARD_Slot_h_
+#define GUARD_Slot_h_
+
+#include <vector>
+#include <string>
+#include "Implant.h"
+ // slot order: head, eye, ear, chest, rarm, larm, waist, rwrist, lwrist, leg, rhand, lhand, feet
+class Slot{
+public:
+  typedef std::vector<std::vector<Implant> >::size_type size_type;
+  typedef std::vector<std::vector<Implant> >::const_iterator const_iterator;
+
+  Slot();
+  Slot(std::string n) : n(n) {}
+
+  void add(Implant i){ limps[limps.size() - 1].push_back(i); }
+  void addVec(){ limps.push_back(std::vector<Implant>()); }
+  const std::string& name() const { return n; }
+  size_type size() const { return limps.size(); }
+
+  const std::vector<Implant>& operator[](int i) const { return limps[i]; }
+
+  const_iterator begin() const { return limps.begin(); }
+  const_iterator end() const { return limps.end(); }
+
+private:
+  std::string n;
+  // this slot has a vector of implant vectors.
+  // implants within the same implant vector have the same cluster specification, but different abi reqs.
+  std::vector<std::vector <Implant> > limps;
+};
+
+#endif
