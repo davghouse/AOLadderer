ec682eecff437d34e21ec56b9e7aee4b46fc0708
David Galehouse <davghouse@gmail.com>
2018-09-01 14:13:59 -0400

    Rename exe to "AO Ladderer"

--- AOLadderer.UI/Properties/AssemblyInfo.cs
+++ AOLadderer.UI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AOLadderer.UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AO Ladderer")]
+[assembly: AssemblyDescription("AO Ladderer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AOLadderer.UI")]
+[assembly: AssemblyProduct("AO Ladderer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
