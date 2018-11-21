a36c314c26ffa8b401d7233ab25c865ec5ff24dd
galehouse5 <galehouse.5@gmail.com>
2018-11-21 17:49:42 -0500

    Maintained build selection state when changing tabs.

---
+++ AOLadderer.Blazor/AppState.cs
@@ -0,0 +1,11 @@
+﻿using AOLadderer.Blazor.Models;
+
+namespace AOLadderer.Blazor
+{
+    public static class AppState
+    {
+        public static ImplantsModel Implants { get; set; } = new ImplantsModel();
+        public static StatsModel Stats { get; set; } = new StatsModel();
+        public static BuffsModel Buffs { get; set; } = new BuffsModel();
+    }
+}
